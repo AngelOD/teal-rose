2ab189c5a948c1e756a0302492d3cd2b08273f55
Tristan Bendixen <angelod1981@gmail.com>
2018-04-16 17:12:03 +0200

    Working on the build script.

--- magefile.go
+++ magefile.go
@@ -2,3 +2,104 @@
 
 package main
 
+import (
+	"fmt"
+	"os"
+	"path/filepath"
+	"runtime"
+	"strings"
+
+	"github.com/magefile/mage/mg"
+	"github.com/magefile/mage/sh"
+)
+
+// for the current OS and architecture
+func Build() {
+	doBuild(runtime.GOOS, runtime.GOARCH)
+}
+
+// for all supported operating systems and architectures
+func BuildAll() {
+	mg.Deps(BuildAll32)
+	mg.Deps(BuildAll64)
+}
+
+func BuildAll64() {
+	mg.Deps(BuildLinux)
+	mg.Deps(BuildWindows)
+}
+
+func BuildAll32() {
+	mg.Deps(BuildLinux32)
+	mg.Deps(BuildWindows32)
+}
+
+// for the amd64 architecture on Linux
+func BuildLinux() {
+	mg.Deps(doGenerate)
+	mg.Deps(Test)
+	doBuild("linux", "amd64")
+}
+
+// for the 386 architecture on Linux
+func BuildLinux32() {
+	mg.Deps(doGenerate)
+	mg.Deps(Test)
+	doBuild("linux", "386")
+}
+
+// for the amd64 architecture on Windows
+func BuildWindows() {
+	mg.Deps(doGenerate)
+	mg.Deps(Test)
+	doBuild("windows", "amd64")
+}
+
+// for the 386 architecture on Windows
+func BuildWindows32() {
+	mg.Deps(doGenerate)
+	mg.Deps(Test)
+	doBuild("windows", "386")
+}
+
+// runs all tests
+func Test() {
+	fmt.Println("Running tests...")
+
+	sh.RunV("go", "test", "-v")
+}
+
+func doBuild(pOs string, pArch string) {
+	fmt.Printf("Building for %s (%s)\n", pOs, pArch)
+
+	path := filepath.Join(getOutputPath(), fmt.Sprintf("%s_%s", pOs, pArch))
+	if strings.ToLower(pOs) == "windows" {
+		path += ".exe"
+	}
+
+	fmt.Printf("Outputting to: %s\n", path)
+
+	os.Setenv("GOOS", pOs)
+	os.Setenv("GOARCH", pArch)
+
+	sh.RunV("go", "build", "-o", path)
+}
+
+func doGenerate() {
+	sh.RunV("go", "generate")
+}
+
+func getOutputPath() string {
+	dir, err := os.Getwd()
+	if err != nil {
+		panic(fmt.Sprintf("Error! %v", err))
+	}
+
+	path := filepath.Join(dir, "_dist")
+
+	if err := os.Mkdir(path, 0755); err != nil && !os.IsExist(err) {
+		panic(fmt.Sprintf("Error! %v", err))
+	}
+
+	return path
+}
